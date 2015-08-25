0bf131237665d6e1907678695c8c229502fe3e9f
joseb <jose.barraza@gmail.com>
2015-08-25 15:03:55 -0500

    Eliminar metodos basura

--- Servicios/Procesamiento/ServiciosRest/ComprasServicio.php
+++ Servicios/Procesamiento/ServiciosRest/ComprasServicio.php
@@ -3,10 +3,6 @@ namespace Tracker\Servicios\ServiciosRest;
 
 class ComprasServicio {
     
-    public static function initService($service) {
-        $service->get("/compras/procesar/","\Tracker\Servicios\ServiciosRest\ComprasServicio","procesar");
-    }
-    
     public static function procesar($p) {
         return (object) array("datos"=>1);
     }
--- Servicios/Procesamiento/ServiciosRest/VisitasServicio.php
+++ Servicios/Procesamiento/ServiciosRest/VisitasServicio.php
@@ -2,9 +2,6 @@
 namespace Tracker\Servicios\ServiciosRest;
 
 class VisitasServicio {
-    public static function initService($service) {
-        $service->get("/visitas/procesar/",__CLASS__,"procesar");
-    }
     
     public static function procesar($p) {
         return (object) array("datos"=>1);
